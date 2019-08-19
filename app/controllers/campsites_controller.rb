87b991fb977f311de4b42097c2818f2108d9267b
Dima Tarasenko <taras3nko.dima@gmail.com>
2019-08-19 17:37:27 +0100

    campsites controller done - search method is in model

--- app/controllers/campsites_controller.rb
+++ app/controllers/campsites_controller.rb
@@ -1,17 +1,22 @@
 class CampsitesController < ApplicationController
   before_action :load_user, only: [:new, :create]
+
   def index
-    query_params = params[:query]
-    location = query_params["location"]
-    start_date = Date.new(query_params["start_date(1i)"].to_i,
-                          query_params["start_date(2i)"].to_i,
-                          query_params["start_date(3i)"].to_i)
-    end_date = Date.new(query_params["end_date(1i)"].to_i,
-                        query_params["end_date(2i)"].to_i,
-                        query_params["end_date(3i)"].to_i)
-    @campsites = Campsite.search(location: location,
-                                 start_date: start_date,
-                                 end_date: end_date)
+    if !params[:query].nil?
+      query_params = params[:query]
+      location = query_params["location"]
+      start_date = Date.new(query_params["start_date(1i)"].to_i,
+                            query_params["start_date(2i)"].to_i,
+                            query_params["start_date(3i)"].to_i)
+      end_date = Date.new(query_params["end_date(1i)"].to_i,
+                          query_params["end_date(2i)"].to_i,
+                          query_params["end_date(3i)"].to_i)
+      @campsites = Campsite.search(location: location,
+                                   start_date: start_date,
+                                   end_date: end_date)
+    else
+      @campsites = Campsite.all
+    end
     # at this point campsites should be an array of suitable objects ready for view.
     # the search method is moved to model so we can add complexity.
   end
