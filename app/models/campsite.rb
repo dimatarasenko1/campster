60685f140583127e9a80ae4d3e75a1b35c22500e
charles geoffroy <charles@dgtlpay.co>
2019-08-19 14:26:27 +0100

    creation of the campsite model with validation presence and amenities inclusion

--- app/models/campsite.rb
+++ app/models/campsite.rb
@@ -1,2 +1,12 @@
 class Campsite < ApplicationRecord
+  AMENITIES = %w(pet\ friendly campfire\ allowed bins toilet)
+  validates :title,
+            :description,
+            :price_per_night,
+            :address,
+            :postcode,
+            :city,
+            :country,
+            :max_guests, presence: true
+  validates :amenities, inclusion: { in: AMENITIES }
 end
