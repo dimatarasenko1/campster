b2f798deb995910e2276b60952b39c5f1a604513
Dima Tarasenko <taras3nko.dima@gmail.com>
2019-08-19 15:03:35 +0100

    added validation of first name and last name at sign up and everything else on update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, , :trackable and :omniauthable
+  validates :first_name, :last_name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :timeoutable
   #email is validated automatically by devise
+
 end
