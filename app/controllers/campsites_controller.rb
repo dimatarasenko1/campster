364a3cca9097d381261d72d573993909f905fb27
charles geoffroy <charles@dgtlpay.co>
2019-08-21 11:48:17 +0100

    Add fonctional map on campsites index

--- app/controllers/campsites_controller.rb
+++ app/controllers/campsites_controller.rb
@@ -3,20 +3,28 @@ class CampsitesController < ApplicationController
 
   def index
     authorize Campsite
-    if !params[:query].nil?
-      query_params = params[:query]
-      location = query_params["location"]
-      start_date = Date.new(query_params["start_date(1i)"].to_i,
-                            query_params["start_date(2i)"].to_i,
-                            query_params["start_date(3i)"].to_i)
-      end_date = Date.new(query_params["end_date(1i)"].to_i,
-                          query_params["end_date(2i)"].to_i,
-                          query_params["end_date(3i)"].to_i)
-      @campsites = Campsite.search(location: location,
-                                   start_date: start_date,
-                                   end_date: end_date)
-    else
-      @campsites = policy_scope(Campsite)
+    # if !params[:query].nil?
+    #   query_params = params[:query]
+    #   location = query_params["location"]
+    #   start_date = Date.new(query_params["start_date(1i)"].to_i,
+    #                         query_params["start_date(2i)"].to_i,
+    #                         query_params["start_date(3i)"].to_i)
+    #   end_date = Date.new(query_params["end_date(1i)"].to_i,
+    #                       query_params["end_date(2i)"].to_i,
+    #                       query_params["end_date(3i)"].to_i)
+    #   @campsites = Campsite.search(location: location,
+    #                                start_date: start_date,
+    #                                end_date: end_date)
+    # else
+    @campsites = policy_scope(Campsite)
+    # end
+    @campsites = Campsite.geocoded #returns campsites with coordinates
+
+    @markers = @campsites.map do |campsite|
+      {
+        lat: campsite.latitude,
+        lng: campsite.longitude
+      }
     end
     # at this point campsites should be an array of suitable objects ready for view.
     # the search method is moved to model so we can add complexity.
