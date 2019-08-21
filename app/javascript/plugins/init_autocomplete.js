0e8f397c6e0f71c6c023d273e9f0cff1213fbd42
Dima Tarasenko <taras3nko.dima@gmail.com>
2019-08-21 13:22:18 +0100

    added ability to override algolia suggestions

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -2,8 +2,11 @@ import places from 'places.js';
 
 const initAutocomplete = () => {
   const addressInput = document.getElementById('campsite-address');
+  addressInput.addEventListener("blur", () => {
+    document.getElementById('algolia-places-listbox-0').style.display = "none";
+  })
   if (addressInput) {
-    places({ container: addressInput });
+    places({ container: addressInput, autocompleteOptions: {autoselect: false} });
   }
 };
 
