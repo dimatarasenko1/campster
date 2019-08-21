67be0c4e0ac799c301aa09b3f87f185a6bab823d
Dima Tarasenko <taras3nko.dima@gmail.com>
2019-08-21 15:48:43 +0100

    added authenticating user before any booking action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,6 @@
 class BookingsController < ApplicationController
   before_action :set_booking, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   def index
     @bookings = policy_scope(Booking)
