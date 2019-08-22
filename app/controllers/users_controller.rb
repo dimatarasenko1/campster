a7709cd976f685554d8ff87f8b5592540a5ddc7a
Dima Tarasenko <taras3nko.dima@gmail.com>
2019-08-22 12:49:34 +0100

    fixed validation on user show controller - you dont need to validate the bookings in this case

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
     @my_bookings = current_user.bookings
-    authorize @my_bookings
-
     @user = User.find(params[:id])
     authorize @user
   end
