7f34c45cdb1b7dbb443b0e50cf7635efd8653428
charles geoffroy <charles@dgtlpay.co>
2019-08-22 13:00:49 +0100

    Modal added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,10 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
+import { initModal } from '../plugins/init_modal';
 
 // Call JS functions
 initMapbox();
 initAutocomplete();
+initModal();
+
