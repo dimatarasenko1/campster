0a5cb3ba9268ea9ec78a3f65d5e409f65805e1e1
Dima Tarasenko <taras3nko.dima@gmail.com>
2019-08-22 12:22:21 +0100

    removed readonly color change

--- app/javascript/plugins/init_flatpicker.js
+++ app/javascript/plugins/init_flatpicker.js
@@ -4,6 +4,7 @@ import "flatpickr/dist/themes/dark.css";
 const initFlatpickr = () => {
   const input = document.querySelector("#query_date-field");
   if (input) {
+    input.removeAttribute("readonly");
     flatpickr(input, {
       mode: "range",
       minDate: "today",
