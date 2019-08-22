6c6585eef0ad7dfc4ef96d685edb3c292d3f84c7
Dima Tarasenko <taras3nko.dima@gmail.com>
2019-08-22 13:46:08 +0100

    added modal functionality - can have multiple modal buttons on one page

--- app/javascript/plugins/init_modal.js
+++ app/javascript/plugins/init_modal.js
@@ -1,29 +1,38 @@
 const initModal = () => {
   // Get the modal
-  var modal = document.getElementById("myModal");
 
   // Get the button that opens the modal
-  var btn = document.getElementById("myBtn");
+  const modals = document.getElementsByClassName("modal");
+  const btns = document.getElementsByClassName("modalBtn");
 
   // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
+  const spans = document.getElementsByClassName("close");
 
   // When the user clicks on the button, open the modal
-  btn.onclick = function() {
-    modal.style.display = "block";
-  }
-
+  [...btns].forEach((btn) => {
+    btn.onclick = () => {
+      const modal = document.getElementsByName(btn.id)[0];
+      console.log("hello")
+      modal.style.display = "block";
+    }
+  });
   // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
+  [...spans].forEach((span) => {
+    span.onclick = () => {
+      [...modals].forEach((modal) => {
+        modal.style.display = "none";
+      })
+    }
+  });
 
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
+    [...modals].forEach((modal) => {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    })
   }
-};
+}
 
 export { initModal };
