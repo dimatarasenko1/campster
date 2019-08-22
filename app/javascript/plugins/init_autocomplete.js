1cee9c324e697597896e5466c08880e3a7c3e282
Dima Tarasenko <taras3nko.dima@gmail.com>
2019-08-22 11:53:17 +0100

    added some padding

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -2,12 +2,12 @@ import places from 'places.js';
 
 const initAutocomplete = () => {
   const addressInput = document.getElementById('query_campsite-address');
-  addressInput.addEventListener("blur", () => {
-    document.getElementById('algolia-places-listbox-0').style.display = "none";
-  })
   if (addressInput) {
+    addressInput.addEventListener("blur", () => {
+      document.getElementById('algolia-places-listbox-0').style.display = "none";
+    })
     places({ container: addressInput, autocompleteOptions: {autoselect: false} });
   }
-};
+}
 
 export { initAutocomplete };
