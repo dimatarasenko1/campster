5e4b9ba99d53ff2ab0a82c9a7d0ab157fec5b71a
charles geoffroy <charles@dgtlpay.co>
2019-08-23 14:37:14 +0100

    new seeds in Ireland

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,6 +27,14 @@ dima.password = "123456"
 dima.password_confirmation = "123456"
 dima.save!
 
+landlord = User.new
+landlord.email = 'landlord@example.com'
+landlord.first_name = "test"
+landlord.last_name = "test"
+landlord.password = "123456"
+landlord.password_confirmation = "123456"
+landlord.save!
+
 puts 'Finished!'
 puts 'Creating campsites...'
 campsites_attributes = [
@@ -39,7 +47,7 @@ campsites_attributes = [
     min_nights: 1,
     address: "PH50 4RJ",
     max_guests: 4,
-    user: dima,
+    user: landlord,
   },
   {
     title: "Blackwater Hostel, Glamping & Campsite",
@@ -50,7 +58,7 @@ campsites_attributes = [
     min_nights: 2,
     address: "PH50 4SG",
     max_guests: 4,
-    user: victor,
+    user: landlord,
   },
   {
     title: "Invercoe caravan & camping park",
@@ -61,7 +69,7 @@ campsites_attributes = [
     min_nights: 2,
     address: "PH49 4HP",
     max_guests: 4,
-    user: victor,
+    user: landlord,
   },
   {
     title: "4 Winds Lakeland Tipis",
@@ -72,7 +80,7 @@ campsites_attributes = [
     min_nights: 2,
     address:"LA22 0JA",
     max_guests: 4,
-    user: dima,
+    user: landlord,
   },
   {
     title: "Low Wray Campsite",
@@ -83,7 +91,7 @@ campsites_attributes = [
     min_nights: 2,
     address:"LA22 0JA",
     max_guests: 3,
-    user: charles,
+    user: landlord,
   },
   {
     title: "BaysBrown Farm Campsite",
@@ -94,7 +102,51 @@ campsites_attributes = [
     min_nights: 2,
     address: "LA22 9JZ",
     max_guests: 4,
-    user: charles,
+    user: landlord,
+  },
+  {
+    title: "Sharamore House",
+    description: "Louis and Terry are happy to host you on their property. A minute's walk from the coastline, this secluded B&B sits 3.5 km from the beaches and hiking trails of Clifden, and 10 km from Connemara National Park.",
+    price_per_night: 20,
+    check_in: "5 PM",
+    check_out: "10 AM",
+    min_nights: 1,
+    address: "Sky Road, Clifden, County Galway, Ireland",
+    max_guests: 6,
+    user: landlord,
+  },
+  {
+    title: "Large field with view on the bay",
+    description: "Mike is pleased to host you on this unique location. Camping location is facing the bay, offering an stocishing view on the sea. Outside of all cities, enjoy the stars during the night (if the weather allows it). The owner offers souvenirs and extra amenities (garbage disposal, wifi)",
+    price_per_night: 10,
+    check_in: "5 PM",
+    check_out: "10 AM",
+    min_nights: 1,
+    address: "Claddaghduff, County Galway, Ireland",
+    max_guests: 4,
+    user: landlord,
+  },
+  {
+    title: "Pasture with exceptional view",
+    description: "Campster exclusive location with view on Ballynakill Lough lake. Daire the owner is happy to invite to rest in this ancient pasture. He also offer to rest in an ancient barn in case of really bad weather. Located at 15km from Clegan, this location is perfect for trekkers travelling along the coast.",
+    price_per_night: 20,
+    check_in: "5 PM",
+    check_out: "10 AM",
+    min_nights: 1,
+    address: "Moyard, Ballynakill, County Galway, Ireland",
+    max_guests: 4,
+    user: landlord,
+  },
+  {
+    title: "Kathleen's lodge - Great sea view",
+    description: "Campster Exclusive - Amazing land next to a lake. Kathleen's, her husband and their 2 dogs will welcome you on their private lands located next to a lake. In addition, Kathleen can provide trekk lunch for an additional price, as well as toilets and garbage disposal amenities. Perfect for trekkers walking along the Forester Coast road.",
+    price_per_night: 10,
+    check_in: "5 PM",
+    check_out: "10 AM",
+    min_nights: 1,
+    address: "Dawros More, Co. Galway, Ireland",
+    max_guests: 4,
+    user: landlord,
   }
 ]
 Campsite.create!(campsites_attributes)
@@ -122,6 +174,22 @@ campsite.remote_photo_url = url
 campsite.save
 campsite = Campsite.offset(1).fifth
 url = "https://res.cloudinary.com/dqh0reqn3/image/upload/v1566386583/camping-5_jbqt6z.jpg"
+campsite.remote_photo_url = url
+campsite.save
+campsite = Campsite.offset(2).fifth
+url = "https://res.cloudinary.com/dqh0reqn3/image/upload/v1566563636/camping-7.png"
+campsite.remote_photo_url = url
+campsite.save
+campsite = Campsite.offset(3).fifth
+url = "https://res.cloudinary.com/dqh0reqn3/image/upload/v1566564938/camping-8.jpg"
+campsite.remote_photo_url = url
+campsite.save
+campsite = Campsite.offset(4).fifth
+url = "https://res.cloudinary.com/dqh0reqn3/image/upload/v1566565833/camping-9.jpg"
+campsite.remote_photo_url = url
+campsite.save
+campsite = Campsite.offset(5).fifth
+url = "https://res.cloudinary.com/dqh0reqn3/image/upload/v1566565833/camping-10.jpg"
 campsite.remote_photo_url = url
 campsite.save
 puts 'Finished!'
